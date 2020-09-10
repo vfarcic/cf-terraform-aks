563d1c9fdb7f8c8f4ed85f7f2ae3d1c8df5e5df6
Viktor Farcic <viktor@farcic.com>
2020-09-10 21:39:01 +0200

    Added ArgoCD

---
+++ get-kubeconfig.sh
@@ -0,0 +1,11 @@
+if [ "$1" == "" ]; then
+  echo "Usage: $0 [CLUSTER_NAME] [RESOURCE_GROUP]"
+  exit
+fi
+
+az aks get-credentials --name $1 --resource-group $2 --file kubeconfig.yaml
+
+echo "Execute the following command to use the newly created Kube config:"
+echo
+echo "export KUBECONFIG=kubeconfig.yaml"
+
