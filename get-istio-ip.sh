0fe1ebdccdd78d75d656bc8affeba4ad1e7c1676
Viktor Farcic <viktor@farcic.com>
2020-11-26 17:18:59 +0100

    Let there be darkness

---
+++ get-istio-ip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while [[ "$ISTIO_HOST" = "" ]]; do
+  export ISTIO_HOST=$(kubectl --namespace istio-system get svc istio-ingressgateway --output jsonpath="{.status.loadBalancer.ingress[0].ip}")
+  sleep 1
+done
+
+echo $ISTIO_HOST
